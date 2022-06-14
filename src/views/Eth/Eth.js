821f820e24f537550796a196addc196177afec2e
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-14 19:36:22 +0530

    feat(View): Eth page

---
+++ src/views/Eth/Eth.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import "./Eth.scss";
+
+function Eth() {
+  return (
+    <div className="EthWrapper">
+      <Header />
+    </div>
+  );
+}
+
+export default Eth;
