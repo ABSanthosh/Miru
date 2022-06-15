68275759b463ae4b9820c2e83c20132303a04264
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-16 00:39:18 +0530

    feat(Eth): List transactions

--- src/views/Eth/Eth.js
+++ src/views/Eth/Eth.js
@@ -1,23 +1,16 @@
 import React, { useEffect, useState } from "react";
-// import Web3 from "web3";
 import Header from "../../components/Header/Header";
 import TabularView from "../../components/TabularView/TabularView";
-import getNBlocks from "../../utils/web3Helper";
+import age from "../../utils/UnixTimestamp";
+import { getNBlocks } from "../../utils/web3Helper";
 import "./Eth.scss";
 
 function Eth() {
   const [blocksList, setBlocksList] = useState([]);
-
-  // const web3 = new Web3(Web3.givenProvider);
-  // const subscription = web3.eth.subscribe("newBlockHeaders");
-
-  // subscription.on("data", async (block, error) => {
-  //   console.log(block.number);
-  //   getNBlocks(10, setBlocksList);
-  // });
+  const [transactionsList, setTransactionsList] = useState([]);
 
   useEffect(() => {
-    getNBlocks(10, setBlocksList);
+    getNBlocks(10, setBlocksList, setTransactionsList);
 
     const timeOut = setInterval(() => {
       getNBlocks(10, setBlocksList);
@@ -38,13 +31,61 @@ function Eth() {
           <TabularView
             type="Latest Blocks"
             rows={blocksList}
-            headers={["Block No.", "Age", "Miner", "Txn"]}
+            headers={["Block", "Age", "Miner", "Txn"]}
             colWidth={["5%", "5%", "23%", "3%"]}
-          />
+          >
+            {blocksList.map((row, index) => {
+              return (
+                <tr
+                  key={index + row.sha3Uncles}
+                  className="TabularViewWrapper__table-row"
+                >
+                  <td className="TabularViewWrapper__table-cell">
+                    <a href={`/block/${row.blockNo}`}>{row.blockNo}</a>
+                  </td>
+                  <td className="TabularViewWrapper__table-cell">
+                    {age(row.timestamp)}
+                  </td>
+                  <td className="TabularViewWrapper__table-cell">
+                    <a href={`/address/${row.miner}`}>{row.miner}</a>
+                  </td>
+                  <td className="TabularViewWrapper__table-cell">
+                    <a href={`/tx/${row.blockNo}`}>{row.transactions.length}</a>
+                  </td>
+                </tr>
+              );
+            })}
+          </TabularView>
         </div>
-        {/* <div className="EthWrapper__container--tableView">
-          <TabularView type="Transactions" />
-        </div> */}
+        <div className="EthWrapper__container--tableView">
+          <TabularView
+            type="Transactions"
+            headers={["Hash", "Block", "From", "", "To"]}
+            colWidth={["10%", "10%", "18%", "3%", "18%"]}
+            rows={transactionsList.slice(0, 10)}
+          >
+            {transactionsList.slice(0, 10).map((row, index) => {
+              return (
+                <tr
+                  key={index + row.blockHash}
+                  className="TabularViewWrapper__table-row"
+                >
+                  <td className="TabularViewWrapper__table-cell">{row.hash}</td>
+                  <td className="TabularViewWrapper__table-cell">
+                    <a href={`/block/${row.blockNumber}`}>{row.blockNumber}</a>
+                  </td>
+                  <td className="TabularViewWrapper__table-cell">
+                    <a href={`/address/${row.from}`}>{row.from}</a>
+                  </td>
+                  <td className="TabularViewWrapper__table-cell">➜</td>
+                  <td className="TabularViewWrapper__table-cell">
+                    <a href={`/address/${row.to}`}>{row.to}</a>
+                  </td>
+                </tr>
+              );
+            })}
+          </TabularView>
+        </div>
       </div>
     </div>
   );
