15be5a862af0592ec7dea8b446225a5324e9cd30
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-17 00:55:26 +0530

    feat(Eth): Standard view for eth

--- src/views/index.js
+++ src/views/index.js
@@ -1,2 +1,4 @@
-export {default as Home} from "./Home/Home"
-export {default as Eth} from "./Eth/Eth"
+export { default as Home } from "./Home/Home";
+
+export { default as Eth } from "./Eth/StandardView/StandardView";
+export { default as EthBlocks } from "./Eth/BlocksView/BlocksView";
