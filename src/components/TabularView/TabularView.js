a3f0d26b1f1d77f9f0c70ea1a8abb54742b9e840
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-18 00:56:20 +0530

    refactor(Eth): View more button

--- src/components/TabularView/TabularView.js
+++ src/components/TabularView/TabularView.js
@@ -2,10 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./TabularView.scss";
 
-function TabularView({ type, headers, rows, colWidth, children }) {
+function TabularView({ type, headers, colWidth, children, viewMore }) {
   return (
     <div className="TabularViewWrapper">
-      <p className="TabularViewWrapper__title">{type}</p>
+      <div className="TabularViewWrapper__header">
+        <p className="TabularViewWrapper__title">{type}</p>
+        {viewMore !== "" && <a href={viewMore}>View all</a>}
+      </div>
       <table className="TabularViewWrapper__table">
         <colgroup>
           {colWidth.map((width, index) => (
@@ -30,14 +33,14 @@ function TabularView({ type, headers, rows, colWidth, children }) {
 TabularView.propTypes = {
   type: PropTypes.string.isRequired,
   headers: PropTypes.arrayOf(PropTypes.string),
-  rows: PropTypes.arrayOf(PropTypes.object),
   colWidth: PropTypes.arrayOf(PropTypes.string),
   children: PropTypes.node.isRequired,
+  viewMore: PropTypes.string,
 };
 TabularView.defaultProps = {
   headers: [],
-  rows: [],
   colWidth: ["5%", "5%", "23%", "3%"],
+  viewMore: "",
 };
 
 export default TabularView;
