88f321ac07aef84c4ed52c881192d107fd31e292
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-19 19:33:14 +0530

    feat(Address): Added data to panel

--- src/views/Eth/AddressView/AddressView.js
+++ src/views/Eth/AddressView/AddressView.js
@@ -4,25 +4,50 @@ import Header from "../../../components/Header/Header";
 import "./AddressView.scss";
 import Tantum from "../../../utils/Tatum";
 import GraphView from "../../../components/GraphView/GraphView";
+import {
+  getBalance,
+  getStorageAt,
+  getTransactionCount,
+  round,
+} from "../../../utils/web3Helper";
+import Web3 from "web3";
 
 function AddressView() {
   const { address } = useParams();
   const tantum = new Tantum();
-  // eslint-disable-next-line no-unused-vars
-  const [addressData, setAddressData] = useState([]);
   const [d3LinkedList, setD3LinkedList] = useState([]);
   const [nodes, setNodes] = useState([]);
   const [currentNode, setCurrentNode] = useState(null);
+
+  const [nodeBalance, setNodeBalance] = useState(0);
+  const [txnCount, setTxnCount] = useState(0);
+  const [storage, setStorage] = useState("");
 
   const data = {
     nodes: nodes,
     links: d3LinkedList,
   };
+  useEffect(() => {
+    // tantum.getTransactions(address, () => {}, setD3LinkedList, setNodes);
+
+    tantum.getNOffsets(address, setD3LinkedList, setNodes, 5);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
-    tantum.getTransactions(address, setAddressData, setD3LinkedList, setNodes);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (currentNode) {
+      getBalance(currentNode.id, setNodeBalance);
+      getTransactionCount(currentNode.id, setTxnCount);
+      getStorageAt(currentNode.id, 0, setStorage);
+    }
+
+    // cleanup
+    return () => {
+      setNodeBalance(0);
+      setTxnCount(0);
+      setStorage("");
+    };
+  }, [address, currentNode]);
 
   return (
     <div className="AddressViewWrapper MainWrapper">
@@ -37,9 +62,111 @@ function AddressView() {
           <div className="AddressViewWrapper__container__body--left">
             <div className="AddressViewWrapper__panel">
               {currentNode === null ? (
-                <p>Click on a node to view Details</p>
+                <p className="AddressViewWrapper__panel--tempBox">
+                  Click on a node to view Details
+                </p>
               ) : (
-                <p>{currentNode.id}</p>
+                <div className="AddressViewWrapper__panel--data">
+                  <div className="AddressViewWrapper__panel--data__header">
+                    <h3>Address details</h3>
+                    <hr />
+                  </div>
+                  <div className="AddressViewWrapper__panel--data__content">
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Address:</span>
+                      <span className="digits">
+                        <span>{currentNode.id}</span>
+                      </span>
+                    </div>
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Balance:</span>
+                      <span className="digits">
+                        {nodeBalance.ether}&nbsp;ETH
+                      </span>
+                    </div>
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Transactions:</span>
+                      <span className="digits">{txnCount}</span>
+                    </div>
+
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Storage:</span>
+                      <span className="digits">
+                        <span>{storage}</span>
+                      </span>
+                    </div>
+                  </div>
+                  <br />
+                  <div className="AddressViewWrapper__panel--data__header">
+                    <h3>Transaction details</h3>
+                    <hr />
+                  </div>
+                  <div className="AddressViewWrapper__panel--data__content">
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Txn Hash:</span>
+                      <span className="digits">
+                        <span>{d3LinkedList[currentNode.index].txnHash}</span>
+                      </span>
+                    </div>
+
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Block Number:</span>
+                      <span className="digits">
+                        {d3LinkedList[currentNode.index].blockNumber}
+                      </span>
+                    </div>
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Sent Value:</span>
+                      <span className="digits">
+                        {round(
+                          Web3.utils.fromWei(
+                            d3LinkedList[currentNode.index].sentAmt
+                          ),
+                          3
+                        )}
+                        &nbsp;ETH
+                      </span>
+                    </div>
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Txn Fee:</span>
+                      <span className="digits">
+                        <span>
+                          {Web3.utils.fromWei(
+                            d3LinkedList[currentNode.index].gasPrice,
+                            "ether"
+                          ) * parseInt(d3LinkedList[currentNode.index].gas)}
+                        </span>
+                      </span>
+                    </div>
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Nonce:</span>
+                      <span className="digits">
+                        {d3LinkedList[currentNode.index].nonce}
+                      </span>
+                    </div>
+
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Position:</span>
+                      <span className="digits">
+                        {d3LinkedList[currentNode.index].txnPosition}
+                      </span>
+                    </div>
+
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Status:</span>
+                      <span className="digits">
+                        {`${d3LinkedList[currentNode.index].txnStatus}`}
+                      </span>
+                    </div>
+
+                    <div className="AddressViewWrapper__panel--data__content__row">
+                      <span className="label">Type:</span>
+                      <span className="digits">
+                        {d3LinkedList[currentNode.index].txnType}
+                      </span>
+                    </div>
+                  </div>
+                </div>
               )}
             </div>
           </div>
