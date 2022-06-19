e3f77485f12380ac5be4acc1bc0d09510032e433
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-19 19:33:32 +0530

    feat(Graph): Legend

--- src/components/GraphView/GraphView.js
+++ src/components/GraphView/GraphView.js
@@ -6,18 +6,28 @@ import ForceLayout from "react-d3-force-layout";
 function GraphView({ data, setCurrentNode }) {
   return (
     <div className="GraphViewWrapper">
+      <div className="GraphViewWrapper__legend">
+        <div className="GraphViewWrapper__legend__item">
+          <span className="GraphViewWrapper__legend__text">Sender</span>
+          <span className="GraphViewWrapper__legend__circle purple" />
+        </div>
+        <div className="GraphViewWrapper__legend__item">
+          <span className="GraphViewWrapper__legend__text">Receiver</span>
+          <span className="GraphViewWrapper__legend__circle green" />
+        </div>
+      </div>
       <ForceLayout
         nodeLinkObject={data}
         nodeClicked={(d) => {
           setCurrentNode(d);
         }}
         colorFunction={(node) =>
-          ({ 1: "red", 2: "green", 3: "purple" }[node.group])
+          ({ 1: "#33f462", 2: "#f431f4", 3: "purple" }[node.group])
         }
-        // showLabelOnHover={{
-        //   id: "ID-modified",
-        //   group: "Type",
-        // }}
+        showLabelOnHover={{
+          id: "ID-modified",
+          group: "Type",
+        }}
         // connectionStrength={0.01}
         fillType="White"
       />
