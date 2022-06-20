76626f23c86ec8353877a3784372aa7cf852859c
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-20 12:23:04 +0530

    feat(Component): Tool tip added to data card

--- src/components/DataCard/DataCard.js
+++ src/components/DataCard/DataCard.js
@@ -2,10 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./DataCard.scss";
 
-function DataCard({ style, title, children }) {
+function DataCard({ style, title, children, className, toolTip }) {
   return (
-    <div className="DataCardWrapper" style={style}>
-      <div className="DataCardWrapper__title">Block Height</div>
+    <div className={`DataCardWrapper ${className}`} style={style}>
+      <span className="DataCardWrapper__help"></span>
+      <span className="DataCardWrapper__toolTip">
+        <p>{toolTip}</p>
+      </span>
+      <div className="DataCardWrapper__title">{title}</div>
       <div className="DataCardWrapper__content">{children}</div>
     </div>
   );
@@ -15,12 +19,16 @@ DataCard.propTypes = {
   style: PropTypes.object,
   title: PropTypes.string,
   children: PropTypes.any,
+  className: PropTypes.string,
+  toolTip: PropTypes.string,
 };
 
 DataCard.defaultProps = {
   style: {},
   title: "",
   children: "",
+  className: "",
+  toolTip: "",
 };
 
 export default DataCard;
