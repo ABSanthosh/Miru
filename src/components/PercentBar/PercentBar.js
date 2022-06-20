7072c5df2aa8502caaead66abeece98b05bfd42b
ABSanthosh <a.b.santhosh02@gmail.com>
2022-06-20 12:23:24 +0530

    feat(Component): Dynamic color based on percentage

--- src/components/PercentBar/PercentBar.js
+++ src/components/PercentBar/PercentBar.js
@@ -2,12 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./PercentBar.scss";
 
-function PercentBar({ percent }) {
+function PercentBar({ percent, color }) {
   return (
     <div className="PercentBarWrapper">
       <div
         className="PercentBarWrapper__bar"
-        style={{ width: `${percent}%` }}
+        style={{ width: `${percent}%`, backgroundColor: color }}
       />
     </div>
   );
@@ -15,6 +15,11 @@ function PercentBar({ percent }) {
 
 PercentBar.propTypes = {
   percent: PropTypes.number.isRequired,
+  color: PropTypes.string,
+};
+
+PercentBar.defaultProps = {
+  color: "cornflowerblue",
 };
 
 export default PercentBar;
